21d3af1e68b185cae6ff56fcdee8caaf62aab437
Flávio Borges <flyfmb@hotmail.com>
2021-07-05 11:51:34 -0300

    Criado app para heroku close #48

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jm0bgkmpe8l8y)am*$7%52%+s*hwy^2xv$$mmn9)$*!6d6&*_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
