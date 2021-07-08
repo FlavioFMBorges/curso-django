8bf01df3d952061638cc8bf319114bd57799bfb9
Flávio Borges <flyfmb@hotmail.com>
2021-07-08 11:29:36 -0300

    Passado caminho para statics files e media close #77

--- pypro/settings.py
+++ pypro/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'  #para upload de arquivos
+MEDIA_URL = '/media/'  # para upload de arquivos
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
