b32ede1eaad179f08232c310c09d2ab2252e845d
Flávio Borges <flyfmb@hotmail.com>
2021-07-14 15:07:52 -0300

    collectfast_enable = False

--- pypro/settings.py
+++ pypro/settings.py
@@ -139,7 +139,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'  # para upload de arquivos
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-COLLECTFAST_ENABLED = True
+COLLECTFAST_ENABLED = False
 
 # COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
