b0ea994cd8b1f2e99ab1461ef7d633f15346c3aa
Flávio Borges <flyfmb@hotmail.com>
2021-07-20 10:52:07 -0300

    Criar usuario cdn aws close #128

--- pypro/settings.py
+++ pypro/settings.py
@@ -138,12 +138,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'  # para upload de arquivos
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 # STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-MEDIA_URL = '/media/'  # para upload de arquivos
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # COLLECTFAST_ENABLED = True
 
