f54666f01002723ca7147d976a3299681cb02890
Flávio Borges <flyfmb@hotmail.com>
2021-10-12 20:06:53 -0300

    arrumada as variaveis com CONTEXTO DE TEMPLATE em video.html e views close #324

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video={'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 600249135}
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 600249135}
     return render(request, 'aperitivos/video.html', context={'video': video})
