59556dd89a408eea344e8078ee209bf2b6faef58
Flávio Borges <flyfmb@hotmail.com>
2021-10-12 18:10:37 -0300

    Usado bloco template com pagina video.html close #322

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -18,9 +18,10 @@ def test_titulo_video(resp):
 
 
 def test_conteudo_video(resp):
-    assert_contains(resp, '<div style="padding:56.25% 0 0 0;position:relative;">'
+    assert_contains(resp, '<div class="mb-4" style="padding:56.25% 0 0 0;position:relative;">'
                           '<iframe src="https://player.vimeo.com/video/600249135?badge=0&amp;autopause='
                           '0&amp;player_id=0&amp;app_id=58479&amp;h=9513c7690d" frameborder="0" allow="autoplay; fullscreen; '
                           'picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" '
                           'title="AULA 01 - MOTIVA&amp;Ccedil;&amp;Atilde;O &amp;mdash; Curso de Python Gr&amp;aacute;tis.mp4"></iframe>'
                           '</div><script src="https://player.vimeo.com/api/player.js"></script>')
+
