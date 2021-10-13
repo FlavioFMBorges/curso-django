214dfdccafa563b21ae6667ca4340bb57686dff0
Flávio Borges <flyfmb@hotmail.com>
2021-10-13 15:02:04 -0300

    Criado nova pagina usando o slug e estrutura de dados da própria página close #329

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 600249135}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 600249135},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 251497688},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
