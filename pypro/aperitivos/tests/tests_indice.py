5302b1ba302771720b07b82c0588d962bef1c046
Flávio Borges <flyfmb@hotmail.com>
2021-10-28 18:57:05 -0300

    Criar Acesso a Método no Template Close #358

--- pypro/aperitivos/tests/tests_indice.py
+++ pypro/aperitivos/tests/tests_indice.py
@@ -21,13 +21,17 @@ def test_status_code(resp):
     ]
 )
 def test_titulo_video(resp, titulo):
-    assert_contains(resp, titulo)
-#
-#
-# def test_conteudo_video(resp):
-#     assert_contains(resp, '<div class="mb-4" style="padding:56.25% 0 0 0;position:relative;">'
-#                           '<iframe src="https://player.vimeo.com/video/600249135?badge=0&amp;autopause='
-#                           '0&amp;player_id=0&amp;app_id=58479&amp;h=9513c7690d" frameborder="0" allow="autoplay; fullscreen; '
-#                           'picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" '
-#                           'title="AULA 01 - MOTIVA&amp;Ccedil;&amp;Atilde;O &amp;mdash; Curso de Python Gr&amp;aacute;tis.mp4"></iframe>'
-#                           '</div><script src="https://player.vimeo.com/api/player.js"></script>')
+    assert_contains(resp, titulo)\
+
+
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows'
+    ]
+)
+def test_link_video(resp, slug):
+    video_link = reverse('aperitivos:video', args=('slug',))
+    assert_contains(resp, f'href= " {video_link} "')
